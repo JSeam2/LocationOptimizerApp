03573562efe87357fe1d29c1789145b3c2caf1f1
Caffa <wangyeelin@gmail.com>
2017-11-17 16:21:10 +0800

    Added fragments for all 3 pages
    I added 3 blank fragment and linked them to the clicking of the bottom
    navigation menu according to https://www.youtube.com/watch?v=afVktMf_LkE

--- app/src/main/java/com/locationoptimizer/MainActivity.java
+++ app/src/main/java/com/locationoptimizer/MainActivity.java
@@ -3,28 +3,35 @@ package com.locationoptimizer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+//    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.select);
+                case R.id.navigation_select:
+                    transaction.replace(R.id.content, new SelectFragment()).commit();
+//                    mTextMessage.setText(R.string.select);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.see);
+                case R.id.navigation_mapMarker:
+                    transaction.replace(R.id.content, new MapFragment()).commit();
+//                    mTextMessage.setText(R.string.see);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.plan);
+                case R.id.navigation_itinerary:
+                    transaction.replace(R.id.content, new PlansFragment()).commit();
+//                    mTextMessage.setText(R.string.plan);
                     return true;
             }
             return false;
@@ -38,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content, new SelectFragment()).commit();
     }
 
 }
